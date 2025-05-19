4cbb09090913e4689b0f40928ce7b05bcfddc387
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-05-19 12:32:16 +0200

    complete and working version

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-let products = document.querySelectorAll('.product');
-let cart = document.querySelector('.cart');
+const performSearch = () => {
+    // console.log("Search function triggered");
+    const searchInput = document.getElementById('search-item').value.toLowerCase();
+    const products = document.querySelectorAll('.product'); // Récupère tous les produits
+
+    products.forEach(product => {
+        const productName = product.querySelector('h2'); // Récupère le <h2> dans chaque produit
+        if (productName) {
+            const textValue = productName.textContent || productName.innerHTML;
+            if (textValue.toLowerCase().indexOf(searchInput) > -1) {
+                product.style.display = ''; // Affiche le produit
+            } else {
+                product.style.display = 'none'; // Cache le produit
+            }
+        }
+    });
+};
+
+// big things learn in this product is that the code will act in weird
+// if the function and the name of the input are the same 
